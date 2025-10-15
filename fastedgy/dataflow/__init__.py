2523d5e8b6cd7d731dfc22b49041689d6151d596
François Pluchino <francois@krafter.io>
2025-10-15 17:49:41 +0200

    refactor(dataflow): move export logic in dedicated dataflow package

---
+++ fastedgy/dataflow/__init__.py
@@ -0,0 +1,19 @@
+# Copyright Krafter SAS <developer@krafter.io>
+# MIT License (see LICENSE file).
+
+from fastedgy.dataflow.exporter import (
+    export_data,
+    format_value,
+    generate_csv_export,
+    generate_xlsx_export,
+    generate_ods_export,
+)
+
+
+__all__ = [
+    "export_data",
+    "format_value",
+    "generate_csv_export",
+    "generate_xlsx_export",
+    "generate_ods_export",
+]
